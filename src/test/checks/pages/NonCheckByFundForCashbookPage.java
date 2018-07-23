0343a411c1532f2137bd1756706152f210ed7e95
dabidpol <dcabatingan@gmail.com>
2018-07-23 09:18:43 +0800

    commit lang for backup

--- src/test/checks/pages/NonCheckByFundForCashbookPage.java
+++ src/test/checks/pages/NonCheckByFundForCashbookPage.java
@@ -60,6 +60,19 @@ public class NonCheckByFundForCashbookPage extends javax.swing.JPanel {
                 , new Object[]{"typeHandler", new com.rameses.rcp.common.TextColumnHandler()}
             }),
             new com.rameses.rcp.common.Column(new Object[]{
+                new Object[]{"name", "people"}
+                , new Object[]{"caption", "People"}
+                , new Object[]{"width", 100}
+                , new Object[]{"minWidth", 0}
+                , new Object[]{"maxWidth", 0}
+                , new Object[]{"required", false}
+                , new Object[]{"resizable", true}
+                , new Object[]{"nullWhenEmpty", true}
+                , new Object[]{"editable", false}
+                , new Object[]{"textCase", com.rameses.rcp.constant.TextCase.NONE}
+                , new Object[]{"typeHandler", new com.rameses.rcp.common.TextColumnHandler()}
+            }),
+            new com.rameses.rcp.common.Column(new Object[]{
                 new Object[]{"name", "particulars"}
                 , new Object[]{"caption", "Particulars"}
                 , new Object[]{"width", 100}
